b736070c6ff35fbe964ed41093ffd3d2aed1f79f
Priytam <priytam.j.pandey@gmail.com>
2015-11-30 01:26:46 +0530

    Bug fix
    tweet undefined

--- modules/tweet/server/models/Tweet.js
+++ modules/tweet/server/models/Tweet.js
@@ -1,7 +1,6 @@
 /**
  * Created by pjpandey on 11/29/2015.
  */
-'use strict';
 var mongoose = require('mongoose');
 
 // Create a new schema for our tweet data
@@ -15,25 +14,25 @@ var schema = new mongoose.Schema({
     screenname : String
 });
 
-
 // Create a static getTweets method to return tweet data from the db
 schema.statics.getTweets = function(page, skip, callback) {
 
-  var tweets = [],
-      start = (page * 10) + (skip * 1);
+    var tweets = [],
+        start = (page * 10) + (skip * 1);
 
-  // Query the db, using skip and limit to achieve page chunks
-  schema.find({},'twid active author avatar body date screenname',{skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs){
-    if(!err) {
-      tweets = docs;  // We got tweets
-        console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!');
-      tweets.forEach(function(tweet){
-        tweet.active = true; // Set them to active
-      });
-    }
-    // Pass them back to the specified callback
-    callback(tweets);
-  });
+    // Query the db, using skip and limit to achieve page chunks
+    Tweet.find({},'twid active author avatar body date screenname',{skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs){
+        if(!err) {
+            tweets = docs;  // We got tweets
+            console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!');
+            tweets.forEach(function(tweet){
+                tweet.active = true; // Set them to active
+            });
+        }
+        // Pass them back to the specified callback
+        callback(tweets);
+    });
 };
 
 // Return a Tweet model based upon the defined schema
+module.exports = Tweet = mongoose.model('Tweet', schema);
